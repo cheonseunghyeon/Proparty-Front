4971aac5458abb4e780b3dfb390c7cd333772b59
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-05-18 23:11:53 +0900

    Update Realtiem.js

--- src/routes/Realtiem.js
+++ src/routes/Realtiem.js
@@ -8,3 +8,41 @@ const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
 server.listen(PORT,()=>console.log(`서버가 ${PORT} 에서 시작되었어요`))
+
+function Join() {
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+    return (
+      <div className='joinOuterContainer'>
+        <div className='joinInnerContainer'>
+          <h1 className='heading'></h1>
+          <div>
+            <input
+              placeholder='이름'
+              className='joinInput'
+              type='text'
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              placeholder='채팅방'
+              className='joinInput mt-20'
+              type='text'
+              onChange={(event) => setRoom(event.target.value)}
+            />
+          </div>
+          <Link
+            onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <button className={'button mt-20'} type='submit'>
+              가입
+            </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+  export default Join;
+  
